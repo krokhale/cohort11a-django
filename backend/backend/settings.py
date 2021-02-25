2a0edb6748b3b1ad572f7840a8da7d12d02d3b5f
krishna rokhale <krishna.rokhale@gmail.com>
2021-02-24 16:27:04 -0800

    checking in

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',            # add this
-    'rest_framework',         # add this 
+    'rest_framework',         # add this
     'todo',
   ]
 
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 
 
 # we whitelist localhost:3000 because that's where frontend will be served
-CORS_ORIGIN_WHITELIST = (
-     'localhost:3000/'
- )
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://localhost:8080',
+]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,11 +5,16 @@ from django.contrib import admin
 from django.urls import path, include                 # add this
 from rest_framework import routers                    # add this
 from todo import views                            # add this
-        
+
 router = routers.DefaultRouter()                      # add this
 router.register(r'todos', views.TodoView, 'todo')     # add this
-        
+
 urlpatterns = [
-    path('admin/', admin.site.urls),           
+    path('admin/', admin.site.urls),
     path('api/', include(router.urls))                # add this
 ]
+
+# GET api/users/1
+# POST api/users
+# PUT api/users/1
+# api/todo
